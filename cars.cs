ad1ba270a253c18dedfa335627d952776780d527
nacelease <54339008+nacelease@users.noreply.github.com>
2019-10-09 14:30:48 -0500

    Add files via upload

---
+++ cars.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace llllllcars.Data
+{
+    public class cars
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CID { get; set; }
+
+        public string color { get; set; }
+        public float year { get; set; }
+        [ForeignKey("MID")]
+        public manufacturers manuID { get; set; }
+    }
+}
---
+++ manufacturers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace llllllcars.Data
+{
+    public class manufacturers
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MID { get; set; }
+        [Display(Name ="Manufacturers Name")]
+        public string Mname { get; set; }
+        [Display(Name ="Manufacturers Type")]
+        public string Mtype { get; set; }
+    }
+}
